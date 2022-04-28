b6f1710c74c1bc27db325eda0a8dd12bc48f3664
mariana montoya <marimontod@gmail.com>
2022-04-28 11:51:51 -0500

    build(app): add boostrap and set structure folders

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  moduleNameMapper: {
+/*   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
+  }, */
+  moduleNameMapper: {
+    '^lodash-es$': 'lodash'
   },
+  testEnvironment: 'jsdom',
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: [
+    '<rootDir>/src/**/*.spec.ts',
+  ],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    '!**/node_modules/**',
+    '!**/src/**/*.module.ts',
+    '!test/**',
+    '!**/polyfills.ts',
+    '!**/environments/**',
+    '!**/src/setupJest.ts'
+  ]
 };
+
