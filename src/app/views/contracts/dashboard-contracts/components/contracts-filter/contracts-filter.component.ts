f93dfa06e2c655dc154bcf0b040aa33653727401
mariana montoya <marimontod@gmail.com>
2022-04-30 18:18:09 -0500

    feat(app): first section responsive

---
+++ src/app/views/contracts/dashboard-contracts/components/contracts-filter/contracts-filter.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contracts-filter',
+  templateUrl: './contracts-filter.component.html',
+  styleUrls: ['./contracts-filter.component.scss']
+})
+export class ContractsFilterComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
